a73b45780ed7161e77b9fc1862d645371264cfa1
Yash Sharma <yash.amrahs@gmail.com>
2025-06-24 23:38:10 +0530

    UserService - Project Setup

---
+++ app/src/main/java/com/userservice/entities/UserInfoDto.java
@@ -0,0 +1,43 @@
+package com.userservice.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserInfoDto {
+    @JsonProperty("user_id")
+    @NonNull
+    private String userId;
+
+    @JsonProperty("first_name")
+    @NonNull
+    private String firstName;
+
+    @JsonProperty("last_name")
+    @NonNull
+    private String lastName;
+
+    @JsonProperty("phone_number")
+    @NonNull
+    private Long phoneNumber;
+
+    @JsonProperty("email")
+    @NonNull
+    private String email;
+
+    @JsonProperty("profile_pic")
+    private String profilePic;
+}
