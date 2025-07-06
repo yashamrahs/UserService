387a1a6883ef78dc2867933b3424cee1955b3d5a
Yash Sharma <yash.amrahs@gmail.com>
2025-07-07 00:12:13 +0530

    Add User Service Config

--- app/src/main/java/com/userservice/config/UserServiceConfig.java
+++ app/src/main/java/com/userservice/config/UserServiceConfig.java
@@ -1,4 +1,14 @@
 package com.userservice.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserServiceConfig {
+
+    @Bean
+    public ObjectMapper objectMapperInit() {
+        return new ObjectMapper();
+    }
 }
