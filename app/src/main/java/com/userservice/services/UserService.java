65c3553a19f420a9313b0fccc721bdeaf6851807
Yash Sharma <yash.amrahs@gmail.com>
2025-07-07 00:10:43 +0530

    Add User Service

--- app/src/main/java/com/userservice/services/UserService.java
+++ app/src/main/java/com/userservice/services/UserService.java
@@ -1,4 +1,63 @@
 package com.userservice.services;
 
-public class UserService {
+import com.userservice.entities.UserInfo;
+import com.userservice.entities.UserInfoDto;
+import com.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+@Service
+@RequiredArgsConstructor
+public class UserService
+{
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserInfoDto createOrUpdateUser(UserInfoDto userInfoDto) {
+        UnaryOperator<UserInfo> updatingUser = user -> {
+            return userRepository.save(userInfoDto.transformToUserInfo());
+        };
+
+        Supplier<UserInfo> createUser = () -> {
+            return userRepository.save(userInfoDto.transformToUserInfo());
+        };
+
+        UserInfo userInfo = userRepository.findByUserId(userInfoDto.getUserId())
+                .map(updatingUser)
+                .orElseGet(createUser);
+
+        return new UserInfoDto(
+                userInfo.getUserId(),
+                userInfo.getFirstName(),
+                userInfo.getLastName(),
+                userInfo.getPhoneNumber(),
+                userInfo.getEmail(),
+                userInfo.getProfilePic()
+        );
+    }
+
+    public UserInfoDto getUser(UserInfoDto userInfoDto) throws Exception {
+        Optional<UserInfo> userInfoDtoOpt = userRepository.findByUserId(userInfoDto.getUserId());
+
+        if(userInfoDtoOpt.isEmpty()) {
+            throw new Exception("User not found");
+        }
+
+        UserInfo userInfo = userInfoDtoOpt.get();
+
+        return new UserInfoDto(
+                userInfo.getUserId(),
+                userInfo.getFirstName(),
+                userInfo.getLastName(),
+                userInfo.getPhoneNumber(),
+                userInfo.getEmail(),
+                userInfo.getProfilePic()
+        );
+    }
+
 }
